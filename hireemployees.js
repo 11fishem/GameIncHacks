683d8aed19ec02d1fee09c8f6dd86f2543cdbb8d
Matthew Allen Fisher <11fishem@gmail.com>
2018-06-01 18:38:58 -0400

    Update hireemployees.js

--- hireemployees.js
+++ hireemployees.js
@@ -1,45 +1,39 @@
-function timeout() {
-setTimeout(function () {
+///For making games
+var goodNames = ["Hype Lord",
+    "Venture Capitalist",
+    "Nathan Flurry",
+    "Matt",
+    "Vince de Vries",
+    "Richard",
+    "Charles",
+    "Nicholas Kissel",
+    "Winnie",
+    "Chester",
+    "Shane",
+    "MasterPtato",
+    "Devin",
+    "Jeff",
+    "Chris",
+    "Scrub Master",
+    "Karl"
+];
 createGame();
 finishCreateGame(0);
 
-for(i=0; i < 3; i++){
-
-var goodNames = ["Hype Lord",
-"Venture Capitalist",
-"Nathan Flurry",
-"Matt",
-"Vince de Vries",
-"Richard",
-"Charles",
-"Nicholas Kissel",
-"Winnie",
-"Chester",
-"Shane",
-"MasterPtato",
-"Devin",
-"Jeff",
-"Chris",
-"Scrub Master",
-"Karl"];
-var namesAvailable = [];
-var availableIndex = -1;
-document.querySelector('.hireTalentButton').click();
-document.querySelectorAll('.name').forEach(function(e) {
-  var employeeName = e.innerHTML;
-  namesAvailable.push(employeeName);
-});
-
-for(i=0; i <= goodNames.length - 1; i++){
-  name = goodNames[i];
-  availableIndex = namesAvailable.indexOf(name);
-  if(availableIndex != -1) break;
-}
- 
- finishHireTalent(availableIndex);
-var namesAvailable = [];
-var availableIndex = -1;
-};
-    }, 1000);
-}
-timeout();
+function hireSingleEmployee() {
+        var namesAvailable = [];
+        var availableIndex = -1;
+        document.querySelector('.hireTalentButton').click();
+        document.querySelectorAll('.name').forEach(function(e) {
+            var employeeName = e.innerHTML;
+            namesAvailable.push(employeeName);
+        });
+        for (i = 0; i <= goodNames.length - 1; i++) {
+            name = goodNames[i];
+            availableIndex = namesAvailable.indexOf(name);
+            if (availableIndex != -1) break;
+        }
+        finishHireTalent(availableIndex);
+        var namesAvailable = [];
+        var availableIndex = -1;
+    };
