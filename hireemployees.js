298693b7c8211a21fd3ea151cf8af100520b79b5
Matthew Allen Fisher <11fishem@gmail.com>
2018-06-01 18:27:41 -0400

    Update hireemployees.js

--- hireemployees.js
+++ hireemployees.js
@@ -1,12 +1,45 @@
-//WORK IN PROGRESS
+function timeout() {
+setTimeout(function () {
+createGame();
+finishCreateGame(0);
 
-//get the employee hiring options
-var employeeOptions = document.querySelectorAll(".name");
+for(i=0; i < 3; i++){
 
-//loop through and get innerHTML
-[].forEach.call(employeeOptions, function() {
-  employee.innerHTML = '';
+var goodNames = ["Hype Lord",
+"Venture Capitalist",
+"Nathan Flurry",
+"Matt",
+"Vince de Vries",
+"Richard",
+"Charles",
+"Nicholas Kissel",
+"Winnie",
+"Chester",
+"Shane",
+"MasterPtato",
+"Devin",
+"Jeff",
+"Chris",
+"Scrub Master",
+"Karl"];
+var namesAvailable = [];
+var availableIndex = -1;
+document.querySelector('.hireTalentButton').click();
+document.querySelectorAll('.name').forEach(function(e) {
+  var employeeName = e.innerHTML;
+  namesAvailable.push(employeeName);
 });
 
-//We'll have to make a sorted list of the innerHTML values ranked in terms of how good they are, and then have the function pick the best option going down the list
-//TODO
+for(i=0; i <= goodNames.length - 1; i++){
+  name = goodNames[i];
+  availableIndex = namesAvailable.indexOf(name);
+  if(availableIndex != -1) break;
+}
+ 
+ finishHireTalent(availableIndex);
+var namesAvailable = [];
+var availableIndex = -1;
+};
+    }, 1000);
+}
+timeout();
