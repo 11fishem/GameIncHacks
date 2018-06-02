454182da3ac9afa8d1cd9eb1eaf852fb359ebc48
Matthew Allen Fisher <11fishem@gmail.com>
2018-06-02 00:32:56 -0400

    Update hireemployees.js

--- hireemployees.js
+++ hireemployees.js
@@ -1,10 +1,21 @@
 ///For making games
-var goodNames = ["Hype Lord",
+var goodNames = [
+    "Prodigy",
+    "Scotty",
+    "Jeff Basil",
+    "Steve Poz",
+    "Mr. Robot",
+    "Mannuci",
+    "Hype Lord",
+    "Jack Morsey",
     "Venture Capitalist",
     "Nathan Flurry",
     "Matt",
+    "Stanly",
     "Vince de Vries",
+    "Captain Sarah",
     "Richard",
+    "Yang",
     "Charles",
     "Nicholas Kissel",
     "Winnie",
@@ -14,32 +25,74 @@ var goodNames = ["Hype Lord",
     "Devin",
     "Jeff",
     "Chris",
-    "Scrub Master",
-    "Karl"
+    "Lizard Man",
+    "Helena",
 ];
 
 function hireSingleEmployee() {
     var namesAvailable = [];
     var availableIndex = -1;
-    document.querySelector('.hireTalentButton').click();
-    document.querySelectorAll('.name').forEach(function(e) {
-        var employeeName = e.innerHTML;
+    var employeeNames = [];
+    var employeeNames = document.querySelectorAll('.name');
+    console.log(employeeNames);
+    var legthofemployeeNames = employeeNames.length - 1;
+    console.log(legthofemployeeNames);
+    for (i = 0; i < 3; i++) {
+        var employeeNameArray = employeeNames[legthofemployeeNames - i];
+        console.log(employeeNameArray);
+        var employeeName = employeeNameArray.innerHTML;
         namesAvailable.push(employeeName);
-    });
+        console.log(namesAvailable);
+    }
+
     for (i = 0; i <= goodNames.length - 1; i++) {
         name = goodNames[i];
         availableIndex = namesAvailable.indexOf(name);
         if (availableIndex != -1) break;
     }
-    finishHireTalent(availableIndex);
-    var namesAvailable = [];
-    var availableIndex = -1;
+    console.log(availableIndex);
+    if (availableIndex === 1) {
+        finishHireTalent(1);
+    } else if (availableIndex === 2) {
+        finishHireTalent(0);
+    } else {
+        finishHireTalent(-1);
+    }
+    console.log(availableIndex);
 };
 
-function makeGameandHire() {
+function makeGame() {
     createGame();
     finishCreateGame(0);
-    for (i = 0; i < 3; i++) {
-        hireSingleEmployee();
-    }
 };
+
+function MakeandHire() {
+    makeGame();
+    hireThree();
+}
+
+function hireThree() {
+    document.querySelector('.hireTalentButton').click();
+    setTimeout(function() {
+        hireSingleEmployee();
+        document.querySelector('.hireTalentButton').click();
+    }, 1500);
+    setTimeout(function() {
+        hireSingleEmployee();
+        document.querySelector('.hireTalentButton').click();
+    }, 3000);
+    setTimeout(function() {
+        hireSingleEmployee();
+        document.querySelector('.hireTalentButton').click();
+    }, 4500);
+    setTimeout(function() {
+        hireSingleEmployee();
+    }, 6000);
+}
+
+function infiniteHire() {
+    setTimeout(function() {
+        MakeandHire();
+        infiniteHire();
+    }, 10000);
+}
