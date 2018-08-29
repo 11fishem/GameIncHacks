f671d4de3482c4f451e72d23beab8d9b15f805cf
Matthew Allen Fisher <11fishem@gmail.com>
2018-08-28 23:53:38 -0400

    Update hireemployees.js

--- hireemployees.js
+++ hireemployees.js
@@ -5,7 +5,9 @@ function makeGame {
     hireButton.click()
     let employeeList = document.querySelector('tbody')
     let employeeInfo = employeeList.querySelectorAll('.infoHolder')
+    let employee = []
     for( i = 0 ; i < employeeInfo.length ; i++){
-        
+       employeeID = config.employees.findIndex(i=> i.name === employeeInfo[0].querySelector('.label').innerHTML)
+       employee[i] = Object.assign({}, config.employees[employeeID])
     }
 }
