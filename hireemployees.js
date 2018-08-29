6a6418e8c2a56c56b97536043a5d4ca31db65479
Matthew Allen Fisher <11fishem@gmail.com>
2018-08-29 00:00:32 -0400

    Update hireemployees.js

--- hireemployees.js
+++ hireemployees.js
@@ -1,13 +1,17 @@
-function makeGame {
-    let hireButton = document.querySelector('.hireTalentButton')
+function makeGame() {
+    setTimeout(function(){}, 3000);
     createGame()
     finishCreateGame(0)
-    hireButton.click()
-    let employeeList = document.querySelector('tbody')
-    let employeeInfo = employeeList.querySelectorAll('.infoHolder')
-    let employee = []
-    for( i = 0 ; i < employeeInfo.length ; i++){
-       employeeID = config.employees.findIndex(i=> i.name === employeeInfo[0].querySelector('.label').innerHTML)
-       employee[i] = Object.assign({}, config.employees[employeeID])
+    function hireEmployees() {
+        let hireButton = document.querySelector('.hireTalentButton')
+        hireButton.click()
+        let employeeList = document.querySelector('tbody')
+        let employeeInfo = employeeList.querySelectorAll('.infoHolder')
+        let employee = []
+        for( i = 0 ; i < employeeInfo.length ; i++){
+           employeeID = config.employees.findIndex(i=> i.name === employeeInfo[0].querySelector('.label').innerHTML)
+           employee[i] = Object.assign({}, config.employees[employeeID])
+        }
     }
+    hireEmployees()
 }
