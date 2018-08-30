16ad0a7b2d904eeb06c7f91c2562f1a83ab7f5a6
Matthew Allen Fisher <11fishem@gmail.com>
2018-08-29 23:34:51 -0400

    Update suebot.js

--- suebot.js
+++ suebot.js
@@ -5,33 +5,21 @@ function suebot() {
     	document.getElementById("createLawsuitTargets").value = topbitch;
     	console.log(topbitch);
     	finishCreateLawsuit(2);
-    	document.querySelector('.hireTalentButton').click();
-    	finishHireTalent(-1);
-		document.querySelector('.hireTalentButton').click();
-    	finishHireTalent(-1);
-    	document.querySelector('.hireTalentButton').click();
-    	finishHireTalent(-1);
-    	document.querySelector('.hireTalentButton').click();
-    	finishHireTalent(-1);
-    	document.querySelector('.hireTalentButton').click();
-    	finishHireTalent(-1);
-    	document.querySelector('.hireTalentButton').click();
-    	finishHireTalent(-1);
-    	document.querySelector('.hireTalentButton').click();
-    	finishHireTalent(-1);
-    	document.querySelector('.hireTalentButton').click();
-    	finishHireTalent(-1);
-    	document.querySelector('.hireTalentButton').click();
-    	finishHireTalent(-1);
-    	document.querySelector('.hireTalentButton').click();
-    	finishHireTalent(-1);
-    	document.querySelector('.hireTalentButton').click();
-    	finishHireTalent(-1);
-    	document.querySelector('.hireTalentButton').click();
-    	finishHireTalent(-1);
-    	document.querySelector('.hireTalentButton').click();
-    	finishHireTalent(-1);
-        suebot();
+	setTimeout(hireLawyer(10), 1000)
     }, 100);
+}
+function hireLawyer(lawsuitSize) {
+	for(i = 0; i < lawsuitSize; i++){
+	document.querySelector('.hireTalentButton').click();
+	let employeeList = document.querySelector('tbody')
+	let employeeInfo = employeeList.querySelectorAll('.infoHolder')
+	let employee = []
+	for( i = 0 ; i < employeeInfo.length ; i++){
+	   var employeeName = employeeInfo[i].querySelector('.label').innerHTML
+	   let employeeID = config.lawyers.findIndex(i=> i.name === employeeName)
+	   employee[i] = Object.assign({}, config.employees[employeeID])
+	}
+	if(employee[0].workSpeed > employee[1].workSpeed){finishHireTalent(0)} else if(employee[1].workSpeed > employee[0].workSpeed){finishHireTalent(1)} else {finishHireTalent(-1)}
+	}
 }
  suebot();
